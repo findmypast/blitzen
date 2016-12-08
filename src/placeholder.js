148a2a4a7212326de6429326b9fd49a6c85212f3
Neil Crawford <neil.crawford@gmail.com>
2016-12-08 19:51:55 +0000

    Adding eslint and usher updates

--- src/placeholder.js
+++ src/placeholder.js
@@ -1,4 +1,3 @@
-
 module.exports = function add(a, b) {
-  return a+b;
+  return a + b
 }
--- src/placeholder.spec.js
+++ src/placeholder.spec.js
@@ -2,6 +2,6 @@ const add = require('./placeholder')
 
 describe('Given a placeholder file', () => {
   it('Should add two numbers together', () => {
-    expect(add(2,3)).to.equal(5)
+    expect(add(2, 3)).to.equal(5)
   })
 })
